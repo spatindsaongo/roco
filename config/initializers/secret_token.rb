aaf047d483e1c5fac5ed25b40cab21293c7705d6
SAM Patindsaongo <spatindsaongo@gmail.com>
2012-09-03 21:40:14 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roco::Application.config.secret_token = '5f4ec9f010aaa8c9760cefdf785d01aad1ae3660de1b5a6666413f3a2acc7a9adaf640f522be41b6aa8bca1935c9e9ab1aff924c076770eecd9e3d0d76c4ddc2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Roco::Application.config.session_store :cookie_store, :key => '_roco_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Roco::Application.config.session_store :active_record_store
